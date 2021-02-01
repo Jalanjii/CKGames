ef17b6b5fb8c78b22b746863c155d14ca241df4f
Jalanji <30749788+Jalanjii@users.noreply.github.com>
2021-02-02 00:50:53 +0200

    Update Stopwatch.py

--- Stopwatch.py
+++ Stopwatch.py
@@ -1,12 +1,8 @@
-# template for "Stopwatch: The Game"
 import simplegui
-# define global variables
 n = 0
 stopcount = 0
 stopmatch = 0
 
-# define helper function format that converts time
-# in tenths of seconds into formatted string A:BC.D
 def format(t):
     global D
     D = t % 10
@@ -16,7 +12,6 @@ def format(t):
     string = str(A)+':'+str(B)+str(C)+'.'+str(D)
     return string
     
-# define event handlers for buttons; "Start", "Stop", "Reset"
 def start():
     
     timer.start()
@@ -37,29 +32,22 @@ def reset():
     timer.stop()
 
 
-# define event handler for timer with 0.1 sec interval
 def glob():
     global n
     n = n + 1
 
-# define draw handler
 def draws(canvas):
     N = format(n)
     canvas.draw_text(N, (75,105), 26, 'White')
     result = (str(stopmatch) + '/' + str(stopcount))
     canvas.draw_text(result, (160,30), 20, 'White')
     
-# create frame
 frame = simplegui.create_frame('Stopwatch', 200, 200)
 timer = simplegui.create_timer(100, glob)
-# register event handlers
 frame.set_draw_handler(draws)
 frame.add_button('Start', start)
 frame.add_button('Stop', stop)
 frame.add_button('Reset', reset)
 
-# start frame
 
 frame.start()
-
-# Please remember to review the grading rubric
