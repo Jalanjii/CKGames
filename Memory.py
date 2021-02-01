dee1e43f9e081a1338c6751e6cb83d9e62e0143a
Jalanji <30749788+Jalanjii@users.noreply.github.com>
2021-02-02 00:48:13 +0200

    Update Memory.py

--- Memory.py
+++ Memory.py
@@ -1,9 +1,7 @@
-# implementation of card game - Memory
 
 import simplegui
 import random
 
-# helper function to initialize globals
 def new_game():
     global cards, exposed, state, counter
     counter = 0
@@ -56,7 +54,6 @@ def mouseclick(pos):
                 fcard = cards[pos1]
                 state = 1
                 
-# cards are logically 50x100 pixels in size 
 def draw(canvas):
     for card_index in range(len(cards)):
         card_pos = 50 * card_index
@@ -66,17 +63,12 @@ def draw(canvas):
         if exposed[card_index] == False:
             canvas.draw_line((card_pos, 50), (card_pos + 49, 50), 100, 'Green')
 
-# create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 800, 100)
 frame.add_button("Reset", new_game)
 label = frame.add_label('Turn = 0')
 
-# register event handlers
 frame.set_mouseclick_handler(mouseclick)
 frame.set_draw_handler(draw)
 
-# get things rolling
 new_game()
 frame.start()
-
-# Always remember to review the grading rubric
