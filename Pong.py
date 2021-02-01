7165ceaad687d68f331ca210fea81283b73e5615
Jalanji <30749788+Jalanjii@users.noreply.github.com>
2021-02-02 00:49:03 +0200

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -1,9 +1,7 @@
-# Implementation of classic arcade game Pong
 
 import simplegui
 import random
 
-# initialize globals - pos and vel encode vertical info for paddles
 WIDTH = 600
 HEIGHT = 400       
 BALL_RADIUS = 20
@@ -16,12 +14,10 @@ RIGHT = True
 ballacc = .10
 padvelrate = 2
 
-# initialize ball_pos and ball_vel for new bal in middle of table
 ball_pos = [WIDTH / 2, HEIGHT / 2]
 ball_vel = [1, 0]
 
 
-# if direction is RIGHT, the ball's velocity is upper right, else upper left
 def spawn_ball(direction):
     global ball_pos, ball_vel # these are vectors stored as lists
     ball_pos = [WIDTH / 2, HEIGHT / 2]
@@ -33,7 +29,6 @@ def spawn_ball(direction):
     
     
     
-# define event handlers
 def new_game():
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
     global score1, score2 
@@ -50,13 +45,11 @@ def draw(canvas):
     global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
     
      
-    # draw mid line and gutters
     canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
     canvas.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
     canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
         
         
-    # update ball
     ball_pos[0] += ball_vel[0]
     ball_pos[1] += ball_vel[1]
     
@@ -66,11 +59,9 @@ def draw(canvas):
         ball_vel[1] = - ball_vel[1]
     
     
-    # draw ball
     canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "Red", "White")
     
     
-    # update paddle's vertical position, keep paddle on the screen 
     if paddle1_pos + paddle1_vel >= HALF_PAD_HEIGHT:
         if paddle1_pos + paddle1_vel <= HEIGHT - HALF_PAD_HEIGHT:
              paddle1_pos += paddle1_vel
@@ -88,7 +79,6 @@ def draw(canvas):
     
     
     
-    # determine whether paddle and ball collide
     if ball_pos[0] <= BALL_RADIUS + PAD_WIDTH:#left
         if paddle2_pos - HALF_PAD_HEIGHT <= ball_pos[1] <= paddle2_pos + HALF_PAD_HEIGHT:
             ball_vel[0] = - ball_vel[0]
@@ -108,7 +98,6 @@ def draw(canvas):
             spawn_ball(LEFT)
             
             
-    # draw scores
     canvas.draw_text(str(score1), [200,100], 30, "White")
     canvas.draw_text(str(score2), [380,100], 30, "White")
  
@@ -149,7 +138,6 @@ def restart():
 def stop():
     frame.stop()
 
-# create frame
 frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
@@ -158,6 +146,5 @@ frame.add_button('Replay', restart)
 frame.add_button('Exit', stop, 60)
 
 
-# start frame
 new_game()
 frame.start()
